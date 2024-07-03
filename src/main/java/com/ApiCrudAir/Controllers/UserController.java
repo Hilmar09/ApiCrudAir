6d22158f3372b761f5ece1e747f9564a06b3fbce
devjmv <javier161890@gmail.com>
2024-07-03 17:47:02 +0200

    add updateById in UserController

--- src/main/java/com/ApiCrudAir/Controllers/UserController.java
+++ src/main/java/com/ApiCrudAir/Controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +35,10 @@ public class UserController {
     @GetMapping(path = "/{id}")
     public Optional<UserModel> getUserById(@PathVariable("id") Long id) {
         return this.userService.getById(id);
+    }
+
+    @PutMapping
+    public UserModel updatUserModel(@RequestBody UserModel user, Long id) {
+        return this.userService.updateById(user, id);
     }
 }
--- src/main/java/com/ApiCrudAir/service/UserService.java
+++ src/main/java/com/ApiCrudAir/service/UserService.java
@@ -15,17 +15,26 @@ public class UserService {
     @Autowired
     IUserRepository userRepository;
 
-    public ArrayList<UserModel> getUsers(){ 
+    public ArrayList<UserModel> getUsers() {
         return (ArrayList<UserModel>) userRepository.findAll();
     }
 
-    public UserModel saveUser(UserModel user){
+    public UserModel saveUser(UserModel user) {
         return userRepository.save(user);
     }
 
-    public Optional<UserModel> getById(Long id){
+    public Optional<UserModel> getById(Long id) {
         return userRepository.findById(id);
     }
 
+    public UserModel updateById(UserModel request, Long id) {
+        UserModel user = userRepository.findById(id).get();
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+
+        return user;
+    }
 
 }
