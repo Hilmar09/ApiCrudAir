c23c75f6dedc39e980c8370331389fe4383a2a0c
Hilmar09 <hil.cris09@gmail.com>
2024-07-03 21:24:14 +0200

    Test Unitary

---
+++ src/test/java/com/ApiCrudAir/SaveUserTest.java
@@ -0,0 +1,47 @@
+package com.ApiCrudAir;
+
+
+import com.ApiCrudAir.Repositories.IUserRepository;
+import com.ApiCrudAir.models.UserModel;
+import com.ApiCrudAir.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class SaveUserTest {
+
+    @Mock
+    private IUserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+    private UserModel user1;
+
+    @BeforeEach
+    public void setUp() {
+        user1 = new UserModel();
+        user1.setId(1L);
+        user1.setFirstName("Cristina");
+        user1.setLastName("Gonzalez");
+        user1.setEmail("Cris.Gonz@example.com");
+    }
+    @Test
+    public void saveUserTest() {
+        when(userRepository.save(user1)).thenReturn(user1);
+
+        UserModel result = userService.saveUser(user1);
+
+        assertEquals(user1, result);
+        verify(userRepository).save(user1);
+    }
+
+
+}
