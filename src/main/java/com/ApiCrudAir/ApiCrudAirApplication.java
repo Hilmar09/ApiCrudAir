55d34e31889e41a0686e906e3457a86f63b2c8e6
Hilmar09 <hil.cris09@gmail.com>
2024-07-03 12:18:19 +0200

    Inicialized

---
+++ src/main/java/com/ApiCrudAir/ApiCrudAirApplication.java
@@ -0,0 +1,13 @@
+package com.ApiCrudAir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudAirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApiCrudAir/ApiCrudAirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiCrudAir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
