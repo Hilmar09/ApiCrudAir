c160f0c863d1c61497f24bc9ccf4cab139de857a
devjmv <javier161890@gmail.com>
2024-07-04 11:57:38 +0200

    update innesesary import

--- src/main/java/com/ApiCrudAir/Repositories/IUserRepository.java
+++ src/main/java/com/ApiCrudAir/Repositories/IUserRepository.java
@@ -1,7 +1,6 @@
 package com.ApiCrudAir.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ApiCrudAir.models.UserModel;
--- src/test/java/com/ApiCrudAir/SaveUserTest.java
+++ src/test/java/com/ApiCrudAir/SaveUserTest.java
@@ -6,11 +6,9 @@ import com.ApiCrudAir.models.UserModel;
 import com.ApiCrudAir.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
