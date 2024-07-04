dc89766e7ac53f5bbb57365c70566440b2a4dd3b
devjmv <javier161890@gmail.com>
2024-07-04 11:33:58 +0200

    verificate code

--- src/main/java/com/ApiCrudAir/service/UserCommentService.java
+++ src/main/java/com/ApiCrudAir/service/UserCommentService.java
@@ -3,8 +3,6 @@ package com.ApiCrudAir.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ApiCrudAir.Repositories.IUserCommentRepository;
