afdee086aff995e767de20015a9d2521fe81fadd
FranciscoJChCano <franciscojcharnecocano@gmail.com>
2024-07-04 11:10:31 +0200

    add password

--- src/main/java/com/ApiCrudAir/ApiCrudAirApplication.java
+++ src/main/java/com/ApiCrudAir/ApiCrudAirApplication.java
@@ -2,6 +2,7 @@ package com.ApiCrudAir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class ApiCrudAirApplication {
@@ -9,5 +10,9 @@ public class ApiCrudAirApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCrudAirApplication.class, args);
 	}
+
+
+
+
 
 }
